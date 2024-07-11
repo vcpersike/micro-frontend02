64a8d8e42e5901a4ecebcb7bd7dc0a0b8c9ec9cf
vcpersike <vcpersike@gmail.com>
2024-07-11 20:19:14 -0300

    Primeiro commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import federation from '@originjs/vite-plugin-federation'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'microFront02',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Cart': './src/Cart',
+      },
+      shared: ['react', 'react-dom'],
+    })
+  ],
+  build: {
+    modulePreload: false,
+    target: 'esnext',
+    minify: false,
+    cssCodeSplit: false,
+  },
+  server: {
+    port: 3002
+  },
+  preview: {
+    port: 3002
+  }
+})
